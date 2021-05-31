5a8a5a1006bcd536bfcbdb42fd8aa22157f55bd7
Azhar Ali <azharofficial599@gmail.com>
2021-05-31 12:05:14 +0530

    inital-commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ModuleInApp from './src/Container/ModuleInApp/Index';
+
+export default class App extends Component {
+  render() {
+    return <ModuleInApp />;
+  }
+}
